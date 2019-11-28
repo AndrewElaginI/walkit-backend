1ebf907bafba89278fdfcf60e06fa70883034e4a
Andrew Elagin <andrewelagin@Andrews-Mac-mini.local>
2019-11-28 11:29:06 +0200

    Initial commit

---
+++ src/users/users.controller.ts
@@ -0,0 +1,37 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Request,
+  Body,
+} from '@nestjs/common';
+
+@Controller('users')
+export class UsersController {
+  @Get()
+  findAll(): string {
+    return 'Find all users';
+  }
+
+  @Get('/:id')
+  findUser(@Request() req): string {
+    return `Find user by id: ${req.params.id}`;
+  }
+
+  @Post()
+  createUser(@Request() req): string {
+    return `User created ${req.body.login}`;
+  }
+
+  @Put('/:id')
+  updateUser(@Request() req): string {
+    return `User updated ${req.params.id}`;
+  }
+
+  @Delete()
+  deleteUser(@Request() req): string {
+    return `User ${req.params.id} deleted`;
+  }
+}
